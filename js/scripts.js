c8941a3719e41d6da555ff2cc2795627d6ccf613
Dave Denlow and Joshua Rinard <joshua@>
2016-10-18 15:23:13 -0700

    this will add bugfix

--- js/scripts.js
+++ js/scripts.js
@@ -2,14 +2,22 @@ $(document).ready(function() {//after doc is ready, function runs.
 
 
   $(".clickable1").click(function() {//click method runs a new function.
-    $("#card-showing").slideToggle();//manatee toggles
-    $("#card-hidden").slideToggle();
+    console.log("clickable1");
+    $("#card-showing").show();//manatee toggles
+    $("#card-hidden").show();
   });
 
+
   $(".clickable2").click(function() {//click method runs a new function.
-    $("#card2-showing").slideToggle();//manatee toggles
-    $("#card2-hidden").slideToggle();
+    console.log("clickable2");
+    $("#card2-showing").show();//manatee toggles
+    $("#card2-hidden").hide();
   });
+
+  // $("button").click(function() {//p tags clickable
+  //   $("p").show();// clickable shows image
+  // });
+
 
 
 
